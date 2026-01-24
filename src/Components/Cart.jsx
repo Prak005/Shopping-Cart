b638542611759040ed7ee88830db24c3c9ac574c
Prak005 <prakharmallick005@gmail.com>
2026-01-24 22:16:43 +0530

    template cart

---
+++ src/Components/Cart.jsx
@@ -0,0 +1,10 @@
+function Cart(){
+    return (
+        <div className='p-8'>
+            <h1 className='text-3xl font-bold mb-4'>Cart</h1>
+            <p className='text-gray-600'>Your cart is empty</p> 
+        </div>
+    )
+}
+
+export default Cart;
