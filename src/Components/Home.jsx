425b9281a75ef46b96eba124b5392708516decf0
Prak005 <prakharmallick005@gmail.com>
2026-01-24 22:16:56 +0530

    template home page

---
+++ src/Components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <div className='p-8'>
+            <h1 className='text-3xl font-bold mb-4'>Welcome to the Shop!</h1>
+            <p className='text-gray-600'>Browse our products and add them to your cart</p>
+        </div>
+    )
+}
+
+export default Home;
