0fc94251512bd3eccd467b682fa5f5782040948b
Prak005 <prakharmallick005@gmail.com>
2026-02-11 22:32:43 +0530

    display cart count

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,13 +1,15 @@
 import {Link} from 'react-router-dom';
+import {useCart} from './CartContext';
 
 function Navbar(){
+    const {totalItems} = useCart();
     return (
         <nav className='bg-white px-6 py-4 shadow flex gap-6'>
             <Link to='/' className='font-semibold hover:text-blue-600'>Home</Link>
             <Link to='/shop' className='font-semibold hover:text-blue-600'>Shop</Link>
-            <Link to='/cart' className='font-semibold hover:text-blue-600'>Cart</Link>
+            <Link to='/cart' className='font-semibold hover:text-blue-600'>Cart ({totalItems})</Link>
         </nav>
-    )
+    );
 }
 
 export default Navbar;
